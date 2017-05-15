8444e8c0d3f0c510e2004b1e65d310a18ef37989
chenjiawei <chenjiawei@hxkid.com>
2017-05-15 09:53:43 +0800

    upadte 增加路由配置RESTful风格

--- app/router.js
+++ app/router.js
@@ -8,5 +8,9 @@ module.exports = app => {
   app.post('/posts', 'post.create');
   app.delete('/posts/:id', 'post.destroy');
   app.get('/posts/:id', 'post.show');
+
+  // 也可以使用以下配置替换上面posts的请求
+  // 可以参考 https://eggjs.org/zh-cn/basics/router.html  # RESTful 风格的 URL 定义
+  // app.resources('posts', '/posts', 'post');
 };
 
